2a7426a55e3ed5e44bf825852f63f91b380b9c89
Nikita Neganov <nikitaneganov@icloud.com>
2017-09-28 18:00:07 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -12,39 +12,39 @@ function teenager(age) {
   }
 }
 
-/* function ageChecker(age) {
+ function ageChecker(age) {
   if (age < 12) {
-    return 'You are a kid'
+    return 'You are a kid';
   } else if (age > 19){
-    return 'You are a grownup'
+    return 'You are a grownup';
   } else {
-    return 'You are a teenager!'
+    return 'You are a teenager!';
   }
 }
 
 function ternaryTeenager(age) {
-  (age > 13 && age <19) ? (return 'You are a teenager') : (return 'You are not a teenager')
+  (age > 13 && age <19) ? (return 'You are a teenager') : (return 'You are not a teenager');
 }
 
 function switchAge(age) {
   switch(age){
     case 13:
-      return 'You are a teenager'
+      return 'You are a teenager';
     case 14:
-      return 'You are a teenager'
+      return 'You are a teenager';
     case 15:
-      return 'You are a teenager'
+      return 'You are a teenager';
     case 16:
-      return 'You are a teenager'
+      return 'You are a teenager';
     case 17:
-      return 'You are a teenager'
+      return 'You are a teenager';
     case 18:
-      return 'You are a teenager'
+      return 'You are a teenager';
     case 19:
-      return 'You are a teenager'
+      return 'You are a teenager';
     default:
-      return 'You have an age'
+      return 'You have an age';
 
   }
 }
-*/
+
