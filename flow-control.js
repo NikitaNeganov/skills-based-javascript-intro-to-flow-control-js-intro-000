c21e98de01936018aa648f212dc986853e2baedb
Nikita Neganov <nikitaneganov@icloud.com>
2017-09-28 18:01:52 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,7 @@ function teenager(age) {
 }
 
  function ageChecker(age) {
-  if (age < 12) {
+  if (age < 13) {
     return 'You are a kid';
   } else if (age > 19){
     return 'You are a grownup';
